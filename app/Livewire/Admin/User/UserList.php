14fa793ef3ff0504b3def2263308728ca19054f2
ماهان محمودی <mahan001ms@gmail.com>
2024-04-03 16:14:12 +0330

    reset create user validation

--- app/Livewire/Admin/User/UserList.php
+++ app/Livewire/Admin/User/UserList.php
@@ -4,19 +4,27 @@ namespace App\Livewire\Admin\User;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class UserList extends Component
 {
+    #[Rule('required')]
     public $name;
+    #[Rule('required|unique:users,email')]
     public $email;
+    #[Rule('required')]
     public $mobile;
+    #[Rule('required')]
     public $password;
+    #[Rule('required')]
     public $image;
 
     public function saveUser()
     {
+        $this->validate();
+
         User::query()->create([
             'name'=>$this->name,
             'email'=>$this->email,
